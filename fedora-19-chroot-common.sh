513155ffbc96ace6203ef5b695ce475481749f44
David Boddie <david@boddie.org.uk>
2017-07-05 00:49:42 +0200

    Made specific common files for specific Fedora versions. Updated the general common file to refer to Fedora 20. Fixed an error in the setup file.

--- fedora-19-chroot-common.sh
+++ fedora-19-chroot-common.sh
@@ -1,8 +1,8 @@
 # Set the CHROOT environment variable to the location of the chroot to be created:
 export CHROOT=/tmp/fedora-chroot
 # Set the MIRROR environment variable to the location of the release RPM:
-export MIRROR=http://dl.fedoraproject.org/pub/archive/fedora/linux/releases/20/Everything/x86_64/os/Packages/f/
+export MIRROR=http://dl.fedoraproject.org/pub/archive/fedora/linux/releases/19/Everything/x86_64/os/Packages/f/
 # Set the RELEASE_RPM environment variable to the file name of the release RPM:
-export RELEASE_RPM=fedora-release-20-1.noarch.rpm
+export RELEASE_RPM=fedora-release-19-2.noarch.rpm
 # Set the RELEASE_VERSION environment variable to the release required:
-export RELEASE_VERSION=20
+export RELEASE_VERSION=19
--- host-scripts/chroot-setup.sh
+++ host-scripts/chroot-setup.sh
@@ -22,7 +22,7 @@ sudo sed -i s/\$releasever/$RELEASE_VERSION/ $CHROOT/etc/yum.repos.d/*.repo
 
 # Temporarily link the directory in the chroot containing repository keys to
 # a directory in the host file system.
-if [ -e /etc/pki]; then
+if [ -e /etc/pki ]; then
   echo "The /etc/pki directory already exists. Exiting..."
   exit 1
 fi
